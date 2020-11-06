c72cb15b3318b9c892e4be4b57b44895c12ae8fa
Micha <michael.roeder@uni-paderborn.de>
2020-11-06 16:18:01 +0100

    Added the usage of the polite option.

--- src/main/java/org/dice_research/ldspider/adapter/system/SystemAdapter.java
+++ src/main/java/org/dice_research/ldspider/adapter/system/SystemAdapter.java
@@ -19,21 +19,21 @@ import org.slf4j.LoggerFactory;
 
 public class SystemAdapter extends AbstractSystemAdapter {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SystemAdapter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemAdapter.class);
 
-	private final static String LDSPIDER_IMAGE = "dicegroup/ldspider-filesink:latest";
-	protected boolean terminating = false;
+    private final static String LDSPIDER_IMAGE = "dicegroup/ldspider-filesink:latest";
+    protected boolean terminating = false;
 
-	protected String ldSpiderInstance;
+    protected String ldSpiderInstance;
 
-	@Override
-	public void init() throws Exception {
-		super.init();
+    @Override
+    public void init() throws Exception {
+        super.init();
 
-	}
+    }
 
-	@Override
-	public void receiveGeneratedData(byte[] data) {
+    @Override
+    public void receiveGeneratedData(byte[] data) {
         // handle the incoming data as described in the benchmark description
         ByteBuffer buffer = ByteBuffer.wrap(data);
         String sparqlUrl = RabbitMQUtils.readString(buffer);
@@ -67,80 +67,86 @@ public class SystemAdapter extends AbstractSystemAdapter {
             throw new IllegalStateException("Couldn't find necessary parameter value for \""
                     + LDSpiderSystem.numberOfThreads + "\". Aborting.");
         }
-        
-        switch(strategy) {
-        case "b" : {
+
+        Literal politenessLiteral = RdfHelper.getLiteral(systemParamModel, null, LDSpiderSystem.politeness);
+        if (politenessLiteral != null) {
+            long politeness = workerCountLiteral.getLong();
+            LOGGER.info("Using static politeness strategy with {}ms delay.", politeness);
+            envVariables.add("polite=" + politeness);
+        }
+
+        switch (strategy) {
+        case "b": {
             // Breadth first strategy - we want to crawl as far as possible
             LOGGER.info("Using breadth-first Strategy");
             envVariables.add("b=100");
             break;
         }
-        case "c" : {
+        case "c": {
             // Load balancing strategy - we want to crawl as many URIs as possible
             LOGGER.info("Using load balanced Strategy");
-            envVariables.add( "c="+Integer.MAX_VALUE);
+            envVariables.add("c=" + Integer.MAX_VALUE);
             break;
         }
-        case "dbfq" : {
+        case "dbfq": {
             // Disk-based breadth first strategy
             LOGGER.info("Using disk-based breadth-first Strategy");
-            envVariables.add( "dbfq=");
+            envVariables.add("dbfq=");
             break;
         }
         default: {
-            throw new IllegalStateException("Got an unknown strategy \""
-                    + LDSpiderSystem.numberOfThreads + "\". Aborting.");
+            throw new IllegalStateException(
+                    "Got an unknown strategy \"" + LDSpiderSystem.numberOfThreads + "\". Aborting.");
         }
         }
-        
         LOGGER.info("Starting LDSpider - FileSink");
         ldSpiderInstance = createContainer(LDSPIDER_IMAGE, CONTAINER_TYPE_SYSTEM,
                 envVariables.toArray(new String[envVariables.size()]));
         LOGGER.info("Image Started");
-	}
+    }
 
-	@Override
-	public void receiveGeneratedTask(String taskId, byte[] data) {
-		throw new IllegalStateException("Should not receive any tasks.");
-	}
+    @Override
+    public void receiveGeneratedTask(String taskId, byte[] data) {
+        throw new IllegalStateException("Should not receive any tasks.");
+    }
 
-	@Override
-	protected synchronized void terminate(Exception cause) {
-		LOGGER.debug("Terminating");
-		terminating = true;
-		super.terminate(cause);
-	}
+    @Override
+    protected synchronized void terminate(Exception cause) {
+        LOGGER.debug("Terminating");
+        terminating = true;
+        super.terminate(cause);
+    }
 
-	@Override
-	public void receiveCommand(byte command, byte[] data) {
-		if (command == Commands.DOCKER_CONTAINER_TERMINATED) {
-			ByteBuffer buffer = ByteBuffer.wrap(data);
-			String containerName = RabbitMQUtils.readString(buffer);
-			int exitCode = buffer.get();
-			containerStopped(containerName, exitCode);
-		}
-		super.receiveCommand(command, data);
-	}
+    @Override
+    public void receiveCommand(byte command, byte[] data) {
+        if (command == Commands.DOCKER_CONTAINER_TERMINATED) {
+            ByteBuffer buffer = ByteBuffer.wrap(data);
+            String containerName = RabbitMQUtils.readString(buffer);
+            int exitCode = buffer.get();
+            containerStopped(containerName, exitCode);
+        }
+        super.receiveCommand(command, data);
+    }
 
-	public void containerStopped(String containerName, int exitCode) {
-		// Check whether it is one of your containers and react accordingly
-		if ((ldSpiderInstance != null) && (ldSpiderInstance.equals(containerName)) && !terminating) {
-			Exception e = null;
-			if (exitCode != 0) {
-				// The ldspider had an error. Its time to panic
-				LOGGER.error("ldspider terminated with exit code {}.", exitCode);
-				e = new IllegalStateException("ldspider terminated with exit code " + exitCode + ".");
-			}
-			ldSpiderInstance = null;
-			terminate(e);
-		}
+    public void containerStopped(String containerName, int exitCode) {
+        // Check whether it is one of your containers and react accordingly
+        if ((ldSpiderInstance != null) && (ldSpiderInstance.equals(containerName)) && !terminating) {
+            Exception e = null;
+            if (exitCode != 0) {
+                // The ldspider had an error. Its time to panic
+                LOGGER.error("ldspider terminated with exit code {}.", exitCode);
+                e = new IllegalStateException("ldspider terminated with exit code " + exitCode + ".");
+            }
+            ldSpiderInstance = null;
+            terminate(e);
+        }
 
-	}
+    }
 
-	@Override
-	public void close() throws IOException {
-		LOGGER.info("Finishing LDSpider File Sink adapter");
-		super.close();
-	}
+    @Override
+    public void close() throws IOException {
+        LOGGER.info("Finishing LDSpider File Sink adapter");
+        super.close();
+    }
 
 }
